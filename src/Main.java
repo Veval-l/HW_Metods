b0cc612731969b5970c64d0365be634d8dc004dc
Oseeva <fselti1168rus@gmail.com>
2023-06-06 23:39:54 +0700

    начальный коммит.

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        task1();
+        task2();
+        task3();
+
+    }
+    static public void task1() {
+        /**
+         *
+         */
+        System.out.println("Задача 1");
+
+
+
+        System.out.println( );
+    }
+    public static void task2() {
+        /**
+         *
+         */
+        System.out.println("Задача 2");
+
+
+
+        System.out.println( );
+    }
+    static public void task3() {
+        /**
+         *
+         */
+        System.out.println("Задача 3");
+
+
+
+        System.out.println( );
+    }
+
+}
